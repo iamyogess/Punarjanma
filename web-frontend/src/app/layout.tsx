fff475444abd22e94c67f5db227ca7b91c92c3db
Pustam Rai <github.pustam@gmail.com>
2025-07-25 11:41:03 +0545

    hello

--- web-frontend/src/app/layout.tsx
+++ web-frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
           {children}
         </body>
       </html>
+      <Toaster position="top-center" />
     </ReactQueryProvider>
   );
 }
